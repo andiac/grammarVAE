d7736ce971d5f5fdc7642e04a1b3c25342ecda94
Andi Zhang <me@andi.ac>
2018-05-18 01:21:05 +0100

    added trans

--- make_trans_dataset_grammar.py
+++ make_trans_dataset_grammar.py
@@ -1,14 +1,14 @@
 from __future__ import print_function
 import nltk
 import pdb
-import zinc_grammar
+import trans_grammar
 import numpy as np
 import h5py
-import molecule_vae
+import trans_vae
 
 
 
-f = open('data/250k_rndm_zinc_drugs_clean.smi','r')
+f = open('data/2db.cfg','r')
 L = []
 
 count = -1
@@ -17,20 +17,22 @@ for line in f:
     L.append(line)
 f.close()
 
-MAX_LEN=277
-NCHARS = len(zinc_grammar.GCFG.productions())
+MAX_LEN=15
+NCHARS = len(trans_grammar.GCFG.productions())
 
-def to_one_hot(smiles):
+def to_one_hot(transactions):
     """ Encode a list of smiles strings to one-hot vectors """
-    assert type(smiles) == list
+    assert type(transactions) == list
     prod_map = {}
-    for ix, prod in enumerate(zinc_grammar.GCFG.productions()):
+    for ix, prod in enumerate(trans_grammar.GCFG.productions()):
+        print(prod)
         prod_map[prod] = ix
-    tokenize = molecule_vae.get_zinc_tokenizer(zinc_grammar.GCFG)
-    print(tokenize)
-    tokens = map(tokenize, smiles)
-    print(tokens)
-    parser = nltk.ChartParser(zinc_grammar.GCFG)
+    # tokenize = trans_vae.get_trans_tokenizer(trans_grammar.GCFG)
+    tokens = []
+    for transaction in transactions:
+        tokens.append(transaction.split())
+    # tokens = map(string.split, smiles)
+    parser = nltk.ChartParser(trans_grammar.GCFG)
     parse_trees = [parser.parse(t).next() for t in tokens]
     productions_seq = [tree.productions() for tree in parse_trees]
     indices = [np.array([prod_map[prod] for prod in entry], dtype=int) for entry in productions_seq]
@@ -43,15 +45,14 @@ def to_one_hot(smiles):
 
 
 OH = np.zeros((len(L),MAX_LEN,NCHARS))
-#for i in range(0, len(L), 100):
-for i in range(0, 10, 1):
-    print('Processing: i=[' + str(i) + ':' + str(i+10) + ']')
+for i in range(0, len(L), 100):
+    print('Processing: i=[' + str(i) + ':' + str(i+100) + ']')
+    onehot = to_one_hot(L[i:i+100])
+    #print(onehot)
+    OH[i:i+100,:,:] = onehot
+print(L[0])
+print(OH[0])
 
-    print(L[i])
-    onehot = to_one_hot([L[i]])
-    print(onehot)
-    #OH[i:i+10,:,:] = onehot
-
-h5f = h5py.File('zinc_grammar_dataset.h5','w')
+h5f = h5py.File('./data/trans_grammar_dataset.h5','w')
 h5f.create_dataset('data', data=OH)
 h5f.close()
